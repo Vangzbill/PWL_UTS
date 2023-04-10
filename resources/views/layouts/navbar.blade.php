0578ffd3139e90c190d2ecee1378b568e86c53df
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-04-10 19:42:39 +0700

    setup for login & register

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,7 +5,7 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{ url('/') }}" class="nav-link">Home</a>
+            <a href="{{ url('/login') }}" class="nav-link">Login</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
             <a href="{{ url('/logout') }}" class="nav-link">Logout</a>
