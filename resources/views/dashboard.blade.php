57a1b3c2096c72f060cd51595976f189f1e1b207
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2023-04-11 07:57:48 +0700

    DashBoard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.template')
 
 @section('content')
-
+    <div class="card">
+        <h2>Data Anggota Kelompok</h2>
+        <div class="card-group">
+            <div class="card mb-3 mr-5" style="max-width: 540px;">
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                    <img src="" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Sabilla Luthfi Rahmadhan</h5>
+                      <p class="card-text mb-0">NIM : 2141720122</p>
+                      <p class="card-text">No. Absen : 22</p>
+                      <p class="card-text"><small class="text-muted">Mahasiswa Polinema</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card mb-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                    <img src="..." alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Virgy Ferdian Surya F</h5>
+                      <p class="card-text mb-0">NIM : 214172116</p>
+                      <p class="card-text">No. Absen : 27</p>
+                      <p class="card-text"><small class="text-muted">Mahasiswa Polinema</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+    </div>
 @endsection
