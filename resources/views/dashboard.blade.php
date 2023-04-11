560d2089ceaa69ffe9653bf7173f12a00aa51617
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2023-04-11 08:27:43 +0700

    Fixing Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,31 +4,33 @@
     <div class="card">
         <h2>Data Anggota Kelompok</h2>
         <div class="card-group">
-            <div class="card mb-3 mr-5" style="max-width: 540px;">
+            <div class="card border border-primary rounded mb-3 mr-4" style="max-width: 540px;">
                 <div class="row no-gutters">
                   <div class="col-md-4">
-                    <img src="" alt="...">
+                    <img src="{{ asset('assets/img/2141720122.jpg') }}" alt="..." class="img-fluid rounded">
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
-                      <h5 class="card-title">Sabilla Luthfi Rahmadhan</h5>
+                      <h5 class="card-title text text-bold">Sabilla Luthfi Rahmadhan</h5>
+                      <p class="card-text mb-0">Kelas : TI-2B</p>
                       <p class="card-text mb-0">NIM : 2141720122</p>
-                      <p class="card-text">No. Absen : 22</p>
+                      <p class="card-text">Absen : 22</p>
                       <p class="card-text"><small class="text-muted">Mahasiswa Polinema</small></p>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="card mb-3" style="max-width: 540px;">
+              <div class="card border border-success mb-3 rounded" style="max-width: 540px;">
                 <div class="row no-gutters">
                   <div class="col-md-4">
-                    <img src="..." alt="...">
+                    <img src="{{ asset('assets/img/3x4.png') }}" alt="..." class="img-fluid rounded">
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
-                      <h5 class="card-title">Virgy Ferdian Surya F</h5>
-                      <p class="card-text mb-0">NIM : 214172116</p>
-                      <p class="card-text">No. Absen : 27</p>
+                      <h5 class="card-title text text-bold">Virgy Ferdian Surya F</h5>
+                      <p class="card-text mb-0">Kelas : TI-2B</p>
+                      <p class="card-text mb-0">NIM : 2141720116</p>
+                      <p class="card-text">Absen : 27</p>
                       <p class="card-text"><small class="text-muted">Mahasiswa Polinema</small></p>
                     </div>
                   </div>
